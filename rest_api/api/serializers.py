50be755c09a66c43de1b73f85e4adad8f3b3490e
Obed <b.obed@yahoo.com>
2019-02-14 16:34:23 +0100

    Initial Commit

---
+++ rest_api/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from webapp.models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = (
+        'firstname',
+        'lastname',
+
+        )
