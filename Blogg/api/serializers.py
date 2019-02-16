a5318b66750e1861ac019812efad3b5eb2f49f44
Obed <b.obed@yahoo.com>
2019-02-16 22:42:09 +0100

    Blogg

---
+++ Blogg/api/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework import serializers
+from Blogg.models import BlogPost
---
+++ Blogg/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
