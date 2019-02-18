8680e10db367b1ce056cdbd0d640518f01be7a96
Obed <b.obed@yahoo.com>
2019-02-18 16:28:21 +0100

    CRUD Update

--- Blogg/urls.py
+++ Blogg/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import create_post, update_post, delete_post,index
+from .views import create_post, update_post, delete_post,homepage
 app_name = 'Blogg'
 
 
 urlpatterns=[
-    path('',index, name= 'homepage'),
+    path('',homepage, name= 'homepage'),
     path('',create_post, name='create_post'),
     path('delete_post/<int:post_id>',delete_post, name='delete_post'),
 ]
--- Blogg/views.py
+++ Blogg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .models import BlogPost
 from .forms import PostForm
 
-def index(request):
+def homepage(request):
     posts = BlogPost.objects.all()
     return render(request,'Blogg/index.html',{'posts':posts})
 
@@ -16,8 +16,9 @@ def create_post(request):
 
 def delete_post(request, post_id):
     posts = BlogPost.objects.get(pk=post_id)
-    posts.delete()
-    return redirect('index')
+    if request.method == 'POST':
+        posts.delete()
+        return redirect('homepage')
 
 def update_post(request):
     pass
