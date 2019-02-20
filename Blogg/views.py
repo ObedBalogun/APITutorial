405a0934c7387351d6358d752d54ddcb6de002d4
Obed <b.obed@yahoo.com>
2019-02-20 15:40:51 +0100

    CRUD Update

--- Blogg/views.py
+++ Blogg/views.py
@@ -11,15 +11,18 @@ def create_post(request):
 
     if form.is_valid():
         form.save()
-        redirect('index')
-    return render(request,'create_post.html',{'form':form})
+        redirect('Blogg:homepage')
+    return render(request,'Blogg/create_post.html',{'form':form})
 
 def delete_post(request, post_id):
     posts = BlogPost.objects.get(pk=post_id)
     if request.method == 'POST':
         posts.delete()
-        return redirect('homepage')
+        return redirect('Blogg:homepage')
 
 def update_post(request, post_id):
     posts = BlogPost.objects.get(pk=post_id)
+    form = PostForm(request.POST or None, instance=posts)
+    if form.is_valid():
+        form.save()
     return render(request,'Blogg/index.html',{'posts':posts})
