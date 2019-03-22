04d65daa068e1b9b0eb53cbf6d9686c555a0176a
Obed <b.obed@yahoo.com>
2019-03-22 23:34:31 +0100

    detail view

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Question, Choice
 
 def index(request):
     questions= Question.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in questions])
-    return HttpResponse(output)
+    context = {'questions':questions}
+    return render(request,'polls/index.html',context)
+
 def detail(request, question_id):
-      return HttpResponse("This is question {}".format(question_id))
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/details.html', {'question':question})
+
 def results(request, question_id):
     return HttpResponse("The results of question %s are" % question_id)
 def vote(request,question_id):
