cdcf3baab5b4c28f8a2eafb8cd5a94206f483685
Obed <b.obed@yahoo.com>
2019-03-25 16:10:40 +0100

    Vote view update

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
     questions= Question.objects.order_by('-pub_date')[:5]
@@ -15,4 +16,13 @@ def results(request, question_id):
 
     return HttpResponse("The results of question %s are" % question_id)
 def vote(request,question_id):
-    return HttpResponse("You're voting on question %s" %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request, 'polls/details.html',{'question':question,
+                                                     'error_message':"You didn't select a choice"})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
