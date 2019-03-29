4d2f9c4a6c231df60c90e88a55e7ad99a3e29aa1
Obed <b.obed@yahoo.com>
2019-03-29 10:25:06 +0100

    Class Based Views and Tests

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    questions= Question.objects.order_by('-pub_date')[:5]
-    context = {'questions':questions}
-    return render(request,'polls/index.html',context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questions'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/details.html', {'question':question})
+    def get_queryset(self):
+        # return Question.objects.order_by('-pub_date')[:5] #returns last 5 published questions
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = ('polls/results.html')
 
-    return render(request, 'polls/results.html',{'question':question})
 def vote(request,question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice= question.choice_set.get(pk=request.POST['choice'])
     except(KeyError,Choice.DoesNotExist):
-        return render(request, 'polls/details.html',{'question':question,
+        return render(request, 'polls/detail.html', {'question':question,
                                                      'error_message':"You didn't select a choice"})
     else:
         selected_choice.votes +=1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question.id,))) #you should always return an HttpResponseRedirect after successfully dealing with POST data.
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+        #you should always return an HttpResponseRedirect after successfully dealing with POST data.
